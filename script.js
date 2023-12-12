ace2cca188d6702e1859babfedb3761c5d28ba8a
minhnguyen9702 <minhnguyen9702@gmail.com>
2023-12-12 16:10:10 +1100

    add listeners to cells to change on click

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const gameBoard = [
 ["", "", ""]
 ]
 
+const turnNumber = 2;
+
 const $gameBoard = document.querySelector("#gameBoard")
 
 function drawGameBoard() {
@@ -11,16 +13,34 @@ function drawGameBoard() {
     for(let i = 0; i < gameBoard.length; i++) {
         const gameRow = `
             <tr>
-                <td>${gameBoard[i][0]}</td>
-                <td>${gameBoard[i][1]}</td>
-                <td>${gameBoard[i][2]}</td>
+                <td class="cell" data-row="${i}" data-column="${0}">${gameBoard[i][0]}</td>
+                <td class="cell" data-row="${i}" data-column="${1}">${gameBoard[i][1]}</td>
+                <td class="cell" data-row="${i}" data-column="${2}">${gameBoard[i][2]}</td>
             </tr>`;
         $gameBoard.innerHTML += gameRow;
+    };
+    addGameButtonListeners();
+}
+
+
+function addGameButtonListeners() {
+    const cells = document.querySelectorAll(".cell");
+    for (const cell of cells) {
+        cell.addEventListener("click", function() {
+            const row = this.getAttribute("data-row");
+            const column = this.getAttribute("data-column");
+            drawMarker(row, column);
+            drawGameBoard();
+        });
+    };
+}
+
+function drawMarker(row, column) {
+    if (gameBoard[row][column] = "") {
     }
+    gameBoard[row][column] = "X";
 }
 
 drawGameBoard()
-
-const turnNumber = 2;
 
 
